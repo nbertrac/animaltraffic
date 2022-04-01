6a10f119292c56b0136dac9f130f6279d2ed470b
nbertrac <74254629+nbertrac@users.noreply.github.com>
2022-04-01 15:33:54 +0200

    Add files via upload

--- admin.php
+++ admin.php
@@ -11,10 +11,21 @@ $cnn = new Database;
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Administration</title>
-  <link rel="shortcut icon" type="image/x-icon" href="/img/favicon.ico">
+  <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-<a href="menu.php" class="menu_control">Retour a l'acceuil</a>
+<nav>
+        <ul>
+          <li><a href="index.php">Accueil</a></li>
+          <li> <a href="animals.php">Animaux</a> </li>
+          <li> <a href="adminConnect.html">Administration</a></li>
+        </ul>
+        <form action="https://www.paypal.com/donate" method="post" target="_blank">
+          <input type="hidden" name="hosted_button_id" value="CSVQM9UD3PV78" />
+          <input type="image" src="https://www.paypalobjects.com/fr_FR/FR/i/btn/btn_donate_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Bouton Faites un don avec PayPal" />
+          <img alt="" border="0" src="https://www.paypal.com/fr_FR/i/scr/pixel.gif" width="1" height="1" />
+        </form>
+      </nav>
 <?php
         if (isset($_GET['type']) && $_GET['type'] === 'mdf') {
                 $sql = "SELECT * FROM animal";
@@ -42,7 +53,7 @@ $cnn = new Database;
             </div>
             <div>
             <label for="" class="required">Age</label>
-            <input type="text" name="age" maxlength="2" size="2" pattern="[0-9]" placeholder="Age">
+            <input type="text" name="age" maxlength="2" size="2" pattern="[0-9]{1,}" placeholder="Age">
             </div>
             <div>
             <label for="" class="required">Photo</label>
@@ -101,7 +112,7 @@ $cnn = new Database;
  </div>
  <div>
  <label for="" class="required">Age</label>
- <input type="text" name="age" maxlength="2" size="2" pattern="[0-9]" placeholder="Age">
+ <input type="text" name="age" maxlength="2" size="2" pattern="[0-9]{1,}" placeholder="Age">
  </div>
  <div>
  <label for="" class="required">Photo</label>
@@ -126,13 +137,50 @@ $cnn = new Database;
         </div>
       </form>
       </div>';
-        } elseif (!isset($_GET['ajt']) && !isset($_GET['mdf']) && !isset($_GET['spr'])) {
+    } elseif (isset($_GET['type']) && $_GET['type'] === 'doss') {
+        $sql = "SELECT * FROM reservation";
+        $query= $cnn->getData($sql);
+        if (!empty($cnn->getData($sql))) {
+            foreach ($query as $key => $value) {
+              echo "
+              <div class='carte-animal'>  
+              <h3> $value->id_animal</h3>
+              <h4> Nom : $value->nom <h4>
+              <h4> Prenom : $value->prenom <h4>
+              <h4> adresse : $value->adresse <h4>
+              <h4> Telephone : $value->telephone <h4>
+              <h4> Mail : $value->mail <h4>
+              <h4> Date de creation : $value->dc <h4>
+              <a href='$value->ci'>Carte d'identité</a>
+              </div>    
+              ";
+            }
+          } else echo "<h3> Il n'y a pas de reservation</h3>";
+        $html = '<form action="mpn.php" method="post" id="form">
+            <select class="select_control" name="id" id="id" required>
+                <option value="">--Valider un dossier--</option>';
+            foreach($query as $key => $value) {
+                $html .= '<option value="' .$value->id. '">'.$value->id.'</option>';
+            }
+        $html .= '</select><select class="select_control" name="valide" id="valide" required>
+        <option value="">--Valider?--</option>
+        <option value="1">--Oui--</option>
+        <option value="0">--Non--</option></select>';
+
+  $html.='<div>
+    <input class="send_control" type="submit" name=type value="Dossier">
+  </div>
+  </form>
+</div>';
+        echo $html;
+    } elseif (!isset($_GET['ajt']) && !isset($_GET['mdf']) && !isset($_GET['spr']) && !isset($_GET['doss'])) {
             $html = '<form action="admin.php" method="get" id="form">
                 <select class="select_control" name="type" id="type">
-                    <option value="">--Type de modification--</option>
+                    <option value="">--Actions--</option>
                     <option value="ajt">Ajouter</option>
                     <option value="spr">Supprimer</option>
                     <option value="mdf">Modifier</option>
+                    <option value="doss">Dossier</option>
                 </select>
       <div>
         <input class="send_control" type="submit" value="Valider">
@@ -140,6 +188,6 @@ $cnn = new Database;
       </form>
     </div>';
             echo $html;
-        }
+    }
         ?>
 </body>
---
+++ animals.php
@@ -0,0 +1,53 @@
+<?php
+require_once('Database.php');
+$cnn = new Database;
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles.css">
+  <title>Animaux</title>
+</head>
+
+<body>
+  <nav>
+    <ul>
+      <li> <a href="index.php">Accueil</a> </li>
+      <li>Animaux</li>
+      <li> <a href="adminConnect.html">Administration</a></li>
+    </ul>
+    <form action="https://www.paypal.com/donate" method="post" target="_blank">
+          <input type="hidden" name="hosted_button_id" value="CSVQM9UD3PV78" />
+          <input type="image" src="https://www.paypalobjects.com/fr_FR/FR/i/btn/btn_donate_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Bouton Faites un don avec PayPal" />
+          <img alt="" border="0" src="https://www.paypal.com/fr_FR/i/scr/pixel.gif" width="1" height="1" />
+        </form>
+  </nav>
+  <h2>Jetez un oeil à notre magnifique collection d'animaux !</h2>
+  <section class="home-animals">
+
+    <?php
+    $sql = "SELECT * FROM animal";
+    $query= $cnn->getData($sql);
+    if (!empty($cnn->getData($sql))) {
+      foreach ($query as $key => $value) {
+        echo "
+        <div class='carte-animal'><a href='details.php?id=$value->id'>  
+        <h3> $value->nom</h3>
+        <img class='img-home' src='$value->photo' alt='dog'>
+        <h4> age : $value->age ans <h4>
+        </a></div>    
+        ";
+      }
+    } else {
+      echo "<h3> on n'a pas d'animaux pour vous :( désolé </h3>";
+    }
+
+    ?>
+  </section>
+</body>
+
+</html>
